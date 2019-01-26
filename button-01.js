438e659f734fd64865112250fccb05540aea09ec
Xiija <36306194+Xiija@users.noreply.github.com>
2019-01-26 14:17:42 -0800

    Update button-01.js

--- button-01.js
+++ button-01.js
@@ -1,10 +1,14 @@
+// UNCOMMENT TO SEE IN ACTION
+
 window.addEventListener('load', function()
-{    var bodybtn = document.querySelector('button');
-     if(!bodybtn.classList.contains('btn') )
-     {   bodybtn.classList.add("btn");
-         var myElement = document.querySelectorAll("button.btn"); 
-         myElement.forEach(function(el)
-         {  var options =           
+{    var bodybtn = document.querySelectorAll('button');
+     bodybtn.forEach(function(e)
+     {                
+       if(!e.classList.contains('btn') )
+       {   e.classList.add("btn");
+           var myElement = document.querySelectorAll("button.btn"); 
+           myElement.forEach(function(el)
+           {  var options =           
              " width: 100px;" +
             	"height: 30px;" +	
             	"font-size: 15px;" +
@@ -17,5 +21,7 @@ window.addEventListener('load', function()
               "box-shadow: 3px 3px 7px #222, inset -4px -1px 5px #555;" ;
             el.style.cssText = options;
          });      
-     }
+       }
+     });
 })
+
