def828bf8a225502f89f66f41a30feb1797a735b
Xiija <36306194+Xiija@users.noreply.github.com>
2019-01-26 14:33:51 -0800

    Create button-02.js

---
+++ button-02.js
@@ -0,0 +1,25 @@
+window.addEventListener('load', function()
+{    var bodybtn = document.querySelectorAll('button');
+     bodybtn.forEach(function(e)
+     {                
+       if(!e.classList.contains('btn') )
+       {   e.classList.add("btn");
+           var myElement = document.querySelectorAll("button.btn"); 
+           myElement.forEach(function(el)
+           {  var options =           
+             " width: 100px;" +
+            	"height: 30px;" +	
+            	"font-size: 15px;" +
+            	"letter-spacing: .1em;" +
+            	"color: orange;" + 
+            	"text-shadow: 0 3px 0 rgba(255,255,255,.1);" +
+            	"font-weight: bold;" +
+            	"background-image: linear-gradient(#888 5%,#555 10%,#222);" +
+            	"border-radius: 7px;" +
+              "box-shadow: 3px 3px 7px #222, inset -4px -1px 5px #555;" ;
+            el.style.cssText = options;
+         });      
+       }
+     });
+})
+
